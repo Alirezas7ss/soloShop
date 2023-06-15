578965cd9a2d962bd3cb8f63412bc2d73ff04230
Alirezas7ss <ali76rez@gmail.com>
2023-06-15 16:40:58 +0330

    fix-addButton

--- src/components/productsPage/AddButton.tsx
+++ src/components/productsPage/AddButton.tsx
@@ -30,38 +30,38 @@ function AddButton() {
   }
   return (
     <div className="pr-1 ">
-      {timeClick && (
-        <div
-          onMouseEnter={() => SetTimeClick(true)}
-          onMouseLeave={() => SetTimeClick(false)}
-          className={` flex rounded-md  border-2 border-accent`}
-        >
-          <button
-            disabled={value === 0}
-            className={` ${
-              value === 0 && "hover:bg-transparent disabled:cursor-auto"
-            } flex w-6 items-center justify-center text-foreground hover:bg-accent `}
-            onClick={minusHandler}
-          >
-            <Icons.minus size={15} />
-          </button>
+        <div className={`${ !timeClick && "hidden"}`}>
           <div
-            // style={{ fontSize }}
-            className="flex space-x-3 overflow-hidden rounded  px-2 leading-none text-foreground"
+            onMouseEnter={() => SetTimeClick(true)}
+            onMouseLeave={() => SetTimeClick(false)}
+            className={` flex rounded-md  border-2 border-accent`}
           >
-            <Digit place={1} value={value} />
+            <button
+              disabled={value === 0}
+              className={` ${
+                value === 0 && "hover:bg-transparent disabled:cursor-auto"
+              } flex w-6 items-center justify-center text-foreground hover:bg-accent `}
+              onClick={minusHandler}
+            >
+              <Icons.minus size={15} />
+            </button>
+            <div
+              // style={{ fontSize }}
+              className="flex space-x-3 overflow-hidden rounded  px-2 leading-none text-foreground"
+            >
+              <Digit place={1} value={value} />
+            </div>
+            <button
+              onClick={plusHandler}
+              disabled={value === 3}
+              className={` ${
+                value === 3 && "hover:bg-transparent disabled:cursor-auto"
+              } flex w-6 items-center justify-center text-foreground hover:bg-accent `}
+            >
+              <Icons.plus size={15} />
+            </button>
           </div>
-          <button
-            onClick={plusHandler}
-            disabled={value === 3}
-            className={` ${
-              value === 3 && "hover:bg-transparent disabled:cursor-auto"
-            } flex w-6 items-center justify-center text-foreground hover:bg-accent `}
-          >
-            <Icons.plus size={15} />
-          </button>
         </div>
-      )}
       {!timeClick && (
         <div
           onMouseEnter={() => SetTimeClick(true)}
