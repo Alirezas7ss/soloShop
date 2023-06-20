b098c37ecf035edf21d44f52792a406be2d366f0
Alirezas7ss <ali76rez@gmail.com>
2023-06-20 22:48:25 +0330

    delete-completely-s3

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 import { ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { ALLOWED_FILE_TYPES } from '../config/s3'
-import { env } from "../../env.mjs"
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -16,10 +15,3 @@ export function formatDate(input: string | number): string {
   })
 }
 
-export function absoluteUrl(path: string) {
-  return `${env.NEXT_PUBLIC_APP_URL}${path}`
-}
-
-export function validateFileType(file: File) {
-    return ALLOWED_FILE_TYPES.includes(file.type)
-  }
